f2d3bc57c5c02d97b82560119e41a52068fe83b2
Shaked Lubin <shaked.lubin@gmail.com>
2023-03-29 20:21:19 +0300

    Full ex2/beta1

---
+++ Ex2/Beta1/my_grep.h
@@ -0,0 +1,7 @@
+#ifndef MY_GREP_H
+#define MY_GREP_H
+
+void find_expression_in_file(char* expression, FILE* file);
+
+#endif
+
--- Temp/my_grep.c
+++ Temp/my_grep.c
@@ -25,3 +25,24 @@ int my_grep(char *filename, char *expression) {
     free(line);
     fclose(file);
 }
+
+int main(int argc, char *argv[]) {
+    char* expression;
+    char* file_name;
+    FILE* file;
+
+    if ( argc != 3 ){
+        printf("Error: Wrong number of arguments, expected 3 but got %d \n", argc);
+        exit(1);
+    }
+
+    expression = argv[0];
+    file_name = argv[1];
+    file = fopen(file_name, "r");
+    if ( file == NULL ) {
+        printf("Error: Unable to open the file '%s' \n", file_name);
+        exit(1);
+    }
+
+    find_expression_in_file(expression, file);
+}
