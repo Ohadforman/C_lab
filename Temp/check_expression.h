60654902a82a13b59cfa4dfadb2e724ebf7c9945
Shaked Lubin <shaked.lubin@gmail.com>
2023-05-06 22:15:48 +0300

    Cleanup

--- Temp/check_expression.h
+++
@@ -1,6 +0,0 @@
-#ifndef CHECK_EXPRESSION_H
-#define CHECK_EXPRESSION_H
-
-int check_expression(char *line, char *expression);
-
-#endif
--- Temp/my_grep.h
+++
@@ -1,7 +0,0 @@
-#ifndef MY_GREP_H
-#define MY_GREP_H
-
-int my_grep(char *filename, char *expression);
-
-#endif
-
